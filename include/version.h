cce252f07eed80a2fa1e58310bcf9ed97e9ca649
Rysiek Labus <rysieklabus@gmail.com>
2021-05-14 23:50:00 +0200

    Init

--- include/version.h
+++ include/version.h
@@ -1,10 +1,10 @@
 
 #ifndef BUILD_NUMBER
-  #define BUILD_NUMBER "5"
+  #define BUILD_NUMBER "6"
 #endif
 #ifndef VERSION
-  #define VERSION "v.0.2.210514_5 - 2021-05-14 23:27:54.997013"
+  #define VERSION "v.0.2.210514_6 - 2021-05-14 23:49:23.770696"
 #endif
 #ifndef VERSION_SHORT
-  #define VERSION_SHORT "v.0.2.210514_5"
+  #define VERSION_SHORT "v.0.2.210514_6"
 #endif
