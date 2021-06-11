b9a4d41279b7ebe42ff0b364e599801d8f911a2d
Rysiek Labus <rysieklabus@gmail.com>
2021-06-11 23:47:20 +0200

    with logo edition...

--- include/version.h
+++ include/version.h
@@ -3,11 +3,11 @@
   #define BUILD_NUMBER "248"
 #endif
 #ifndef VERSION
-  #define VERSION "v.1.2.4 - 20210610"
+  #define VERSION "v.1.2.4 - 20210611"
 #endif
 #ifndef VERSION_SHORT
   #define VERSION_SHORT "v.1.2.4"
 #endif
 #ifndef BUILD_DATE
-  #define BUILD_DATE "20210610"
+  #define BUILD_DATE "20210611"
 #endif
